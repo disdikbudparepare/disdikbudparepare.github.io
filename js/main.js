18945febfafa84d12a1828864092b98e0aebe5ae
hisayuki <syuaibsyuaib@gmail.com>
2020-01-11 02:17:57 +0800

    login page
    sisa percobaan 5 kali

---
+++ js/main.js
@@ -0,0 +1,23 @@
+let alamat = "https://script.google.com/macros/s/AKfycbwZ2wKOLBvcdWRtmDtG-NDhIxMVLwh-HnFcDOfc7AteUT5aKIZd/exec";
+let main = {
+    kirimdata: function(dataku) {
+        return fetch(alamat, {
+            method: 'post',
+            body: JSON.stringify(dataku)
+        }).then((response) => {
+            return response.json();
+        })
+    },
+    simpanNip: function(nipPegawai) {
+        return localStorage.setItem("nip", nipPegawai);
+    },
+    ambilNip: function() {
+        return localStorage.getItem("nip");
+    },
+    cekLogin: function() {
+        return fetch(alamat + `?nip=${this.ambilNip()}`)
+            .then((data) => {
+                return data.text()
+            })
+    }
+}
